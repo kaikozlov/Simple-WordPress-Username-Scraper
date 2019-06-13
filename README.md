642aaba403c463cad16a11e45a5f76c207a0d804
Kai Kozlov <37962720+kaikozlov@users.noreply.github.com>
2019-06-13 15:49:10 -0500

    added inputs and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Simple-WordPress-Username-Scraper
 A clean and concise WordPress scraper written in Python 3
+
+Requirements:
+    Python 3
+    BeautifulSoup4
--- scraper.py
+++ scraper.py
@@ -1,29 +1,28 @@
-# import libraries
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import csv
 
+wpsite = input('Enter WordPress Site: ')
+unum = int(input('Enter Start User: '))
 #specify WordPress URL
-quote_page = "https://wordpressurl.tld/?author="
+quote_page = wpsite + "/?author="
 
 #establish author number variable
-author = 2
-
+author = unum
 while True:
     author = author+1
-    #query the website and return the html to the variable "page"
+    #query the website and return html to the variable "page"
     page = urlopen(Request(quote_page + str(author), headers={'User-Agent': 'Mozilla/5.0'}))
-	
 
     soup = BeautifulSoup(page, 'html.parser')
 
-    # Take out the <div> of name and get its value
+    #Take out the <div> of name and get its value
     name_box = soup.find('meta', attrs={'name':'description'})
     print(name_box)
     print(quote_page + str(author))
 
-    # open a csv file with append, so old data will not be erased
+    #open a csv file with append, so old data will not be erased
     with open('index.csv', 'a') as csv_file:
         writer = csv.writer(csv_file)
-        
+
         writer.writerow([name_box, quote_page + str(author)])
